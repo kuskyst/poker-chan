8eb11f8eb76c2433ad5d19f27551d92ed263e4fc
kohsaka <yoshito_kosaka@aiful.co.jp>
2024-11-18 17:22:42 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <template>
   <v-container>
+    <v-text-field label="title" variant="outlined" clearable />
+    <v-row justify="center" align-items="center">
+      <v-col>
+        <v-text-field label="your name" variant="outlined" v-model="yourName" />
+      </v-col>
+      <v-col>participants: {{ participants.join(', ') }}</v-col>
+    </v-row>
+
     <v-btn color="blue" @click="reveal">Reveal</v-btn>
-    <v-btn color="red" @click="reset">Reset</v-btn>
-    Avarage: {{ average }}
+    <v-btn color="red" class="ma-3" @click="reset">Reset</v-btn>
+    Average: {{ average }}
     <v-row justify="center">
-      <v-col cols="16" class="d-flex justify-center">
-        <v-sheet @drop.prevent="onDrop" @dragover.prevent rounded="xl" color="green-lighten-2" width="100%" height="400">
-          <div v-for="(card, index) in selectedCards" :key="index" class="card-stack" :style="getCardStackStyle(index)">
+      <v-col class="justify-center">
+        <v-sheet @drop.prevent="onDrop" @dragover.prevent rounded="xl" color="green-lighten-2" width="100%" height="500px">
+          <div v-for="(card, index) in selectedCards" :key="index" :style="getCardStackStyle(index)">
             <v-card height="135" width="80">
               <v-card-title class="text-end">{{ isOpen ? card : '??' }}</v-card-title>
               <v-card-subtitle v-if="card % 2 == 0" class="text-center">❤︎♦️<br>♧♤</v-card-subtitle>
@@ -42,6 +50,8 @@
 <script setup>
 import { ref } from 'vue';
 
+const yourName = ref('名無しちゃん');
+const participants = ref(['名無しちゃん1', '名無しちゃん2', '名無しちゃん3', '名無しちゃん4', '名無しちゃん5', '名無しちゃん6']);
 const cards = ref([0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100]);
 const selectedCard = ref();
 const selectedCards = ref([]);
@@ -74,21 +84,16 @@ const onDrop = (event) => {
 };
 
 const getCardStackStyle = (index) => {
-  const random1 = Math.floor(Math.random() * (20 + 1 - 30)) + 30;
+  const random1 = Math.floor(Math.random() * (15 + 1 - 30)) + 30;
   const random2 = Math.floor(Math.random() * (45 + 1 - 55)) + 55;
   const random3 = Math.floor(Math.random() * (0 + 1 - 180)) + 180;
   return {
     top: `${random1}%`,
     left: `${random2}%`,
     transform: `rotate(${random3}deg)`,
+    position: `absolute`,
+    transition: `all 0.3s ease`,
     zIndex: index
   };
 };
 </script>
-
-<style scoped>
-.card-stack {
-  position: absolute;
-  transition: all 0.3s ease;
-}
-</style>
