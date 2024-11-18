273fe3e2a608ece133f2be8f82623f279f508f44
kohsaka <yoshito_kosaka@aiful.co.jp>
2024-11-18 12:16:39 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,24 +2,24 @@
   <v-container>
     <v-row justify="center">
       <!-- フィールドエリア -->
-      <v-col cols="12" class="d-flex justify-center">
-        <div class="game-field" @drop.prevent="onDrop" @dragover.prevent>
+      <v-col cols="16" class="d-flex justify-center">
+        <v-sheet @drop.prevent="onDrop" @dragover.prevent rounded="xl" color="pink-lighten-4" width="100%" height="300">
           <!-- ドロップされたカードを表示 -->
           <div v-for="(card, index) in droppedCards" :key="index" class="card-stack" :style="getCardStackStyle(index)">
-            <v-card class="card" :style="cardStyle" @dragEnd="onDragEnd(card, $event)">
-                <v-card-title class="text-end">{{ card }}</v-card-title>
-                <v-card-subtitle v-if="card % 2 == 0" class="text-center">❤︎♦️<br>♧♤</v-card-subtitle>
-                <v-card-subtitle v-else class="text-center">♡♢<br>♣️♠︎</v-card-subtitle>
-                <v-card-title class="text-start">{{ card }}</v-card-title>
+            <v-card height="135" width="80">
+              <v-card-title class="text-end">{{ card }}</v-card-title>
+              <v-card-subtitle v-if="card % 2 == 0" class="text-center">❤︎♦️<br>♧♤</v-card-subtitle>
+              <v-card-subtitle v-else class="text-center">♡♢<br>♣️♠︎</v-card-subtitle>
+              <v-card-title class="text-start">{{ card }}</v-card-title>
             </v-card>
           </div>
-        </div>
+        </v-sheet>
       </v-col>
     </v-row>
 
     <v-row justify="center">
       <v-col v-for="(card, index) in cards" :key="index" cols="auto" class="d-flex justify-center">
-        <v-card class="card" :draggable="true" @dragstart="onDragStart(card, $event)">
+        <v-card class="ma-1" :draggable="true" @dragstart="onDragStart(card, $event)" height="135" width="80">
           <v-card-title class="text-end">{{ card }}</v-card-title>
           <v-card-subtitle v-if="card % 2 == 0" class="text-center">❤︎♦️<br>♧♤</v-card-subtitle>
           <v-card-subtitle v-else class="text-center">♡♢<br>♣️♠︎</v-card-subtitle>
@@ -35,12 +35,6 @@ import { ref } from 'vue';
 
 const cards = ref([0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 40]);
 const droppedCards = ref([]);
-
-const cardStyle = ref({
-  position: 'absolute',
-  cursor: 'pointer',
-  transition: 'all 0.3s ease',
-});
 
 const onDragStart = (card, event) => {
   event.dataTransfer.setData('card', JSON.stringify(card));
@@ -81,24 +75,8 @@ const onDragEnd = (card, event) => {
 </script>
 
 <style scoped>
-.game-field {
-  width: 100%;
-  height: 300px;
-  position: relative;
-  border: 2px dashed #aaa;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-}
-
 .card-stack {
   position: absolute;
   transition: all 0.3s ease;
-}
-
-.card {
-  width: 80px;
-  height: 130px;
-  margin: 5px;
 }
 </style>
