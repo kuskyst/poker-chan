5d561aa4fc270e4a9bfaea01256d4fa60c272d04
kuskyst <126965999+kuskyst@users.noreply.github.com>
2024-11-20 22:45:02 +0900

    apple juice

--- components/score-card.vue
+++ components/score-card.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card v-if="score > 0" height="130" width="90">
-    <v-card-title class="text-end">{{ isOpen ? score : '??' }}</v-card-title>
+    <v-card-title class="text-end">{{ open ? score : '??' }}</v-card-title>
     <v-card-subtitle v-if="score % 2 == 0" class="text-center">♥⬧<br>♧♤</v-card-subtitle>
     <v-card-subtitle v-else class="text-center">♡♢<br>☘♠︎</v-card-subtitle>
-    <v-card-title class="text-start">{{ isOpen ? score : '??' }}</v-card-title>
+    <v-card-title class="text-start">{{ open ? score : '??' }}</v-card-title>
   </v-card>
   <v-card v-else height="130" width="90" class="d-flex align-center justify-center">☕</v-card>
 </template>
@@ -11,6 +11,6 @@
 <script setup lang="ts">
 defineProps<{
   score: number,
-  isOpen: boolean
+  open: boolean
 }>()
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -6,25 +6,25 @@
         <v-col cols="4">
           <v-text-field bg-color="white" label="your name" variant="solo" v-model="yourName" />
         </v-col>
-        <v-col class="text-truncate">participants: {{ participants.join(', ') }}</v-col>
+        <v-col class="text-truncate">members: {{ members.join(', ') }}</v-col>
       </v-row>
-      Vote: {{ stackScores.length }} / {{ participants.length }}
-      <v-btn color="blue" class="ma-3" @click="reveal" prepend-icon="mdi-send" :disabled="participants.length != stackScores.length">Reveal</v-btn>
-      <v-btn color="red" class="ma-3" @click="reset" prepend-icon="mdi-delete" :disabled="stackScores.length == 0">Reset</v-btn>
+      Vote: {{ votes.length }} / {{ members.length }}
+      <v-btn color="blue" class="ma-3" @click="reveal" prepend-icon="mdi-send" :disabled="members.length != votes.length">Reveal</v-btn>
+      <v-btn color="red" class="ma-3" @click="reset" prepend-icon="mdi-delete" :disabled="votes.length == 0">Reset</v-btn>
       Average: {{ average > 0 ? Math.floor((average) * 100) / 100 : '??' }}
     </div>
 
     <v-container>
       <v-sheet class="d-flex" @drop.prevent="onDrop" @dragover.prevent border="xl" rounded="xl" color="green-lighten-2 position-relative" width="100%" height="45vh">
         <v-card class="position-absolute top-0 left-0 bottom-0 right-0 bg-transparent ma-auto" border="surface-light lg" rounded="xl" width="70%" height="70%" />
-        <div class="ma-1 text-white" v-for="(score, index) in stackScores" :key="index" :style="stackScoresStyle(index)">
-          {{ isReveal ? participants[index] : '' }}
+        <div class="ma-1 text-white" v-for="(vote, index) in votes" :key="index" :style="votesStyle(index)">
+          {{ isReveal ? members[index] : '' }}
           <score-card
-            :is-open="isReveal"
-            :score="score"
+            :open="isReveal"
+            :score="vote"
             :class="{
-              'bg-red-lighten-4': isReveal && Math.min(...stackScores.filter(v => v > 0)) == score,
-              'bg-blue-lighten-4': isReveal && Math.max(...stackScores) == score,
+              'bg-red-lighten-4': isReveal && Math.min(...votes.filter(v => v > 0)) == vote,
+              'bg-blue-lighten-4': isReveal && Math.max(...votes) == vote,
               'mt-1': isReveal
             }"
           />
@@ -32,24 +32,24 @@
       </v-sheet>
 
       <v-row class="ml-1 overflow-x-scroll flex-nowrap">
-        <v-col v-for="(score, index) in hands" :key="index" cols="auto" class="d-flex justify-center">
+        <v-col v-for="(hand, index) in hands" :key="index" cols="auto" class="d-flex justify-center">
           <score-card
             draggable="true"
             class="ma-1"
-            :is-open="true"
-            :score="score"
-            @dragstart="onDragStart(score, $event)"
-            @dblclick="play(score)"
+            :open="true"
+            :score="hand"
+            @dragstart="onDrag(hand, $event)"
+            @dblclick="play(hand)"
             :class="{
-              'bg-green-accent-2': selectedScore == score,
-              'text-white': selectedScore == score
+              'bg-green-accent-2': score == hand,
+              'text-white': score == hand
             }"
           />
         </v-col>
         <v-col>
           <v-card height="130" width="90" class="ma-1">
             <v-number-input flat hide-details inset v-model="drawScore" variant="solo" controlVariant="stacked" :max="99" :min="1" />
-            <v-btn elevation="0" height="60%" width="100%" append-icon="mdi-credit-card-plus-outline" @click="draw(drawScore)">add</v-btn>
+            <v-btn elevation="0" height="60%" width="100%" append-icon="mdi-credit-card-plus-outline" @click="draw(drawScore)">draw</v-btn>
           </v-card>
         </v-col>
       </v-row>
@@ -61,37 +61,37 @@
 import { ref, type StyleValue } from 'vue';
 
 const yourName = ref('名無しちゃん');
-const participants = ref(['名無しちゃん1', '名無しちゃん2', '名無しちゃん3', '名無しちゃん4', '名無しちゃん5', '名無しちゃん6']);
+const members = ref(['名無しちゃん1', '名無しちゃん2', '名無しちゃん3', '名無しちゃん4', '名無しちゃん5', '名無しちゃん6']);
 const hands = ref([-1, 0.5, 1, 2, 3, 5, 8, 13, 20, 40, 100]);
-const selectedScore = ref();
-const stackScores = ref<number[]>([]);;
+const score = ref();
+const votes = ref<number[]>([]);;
 const average = ref(0);
 const isReveal = ref(false);
 const drawScore = ref(10);
 
-const play = (score: number) => {
-  if (!isReveal.value && !isNaN(score) && participants.value.length != stackScores.value.length) {
-    stackScores.value.push(score);
-    selectedScore.value = score;
+const play = (card: number) => {
+  if (!isReveal.value && !isNaN(card) && members.value.length != votes.value.length) {
+    votes.value.push(card);
+    score.value = card;
   }
 }
-const draw = (score: number) => {
-  if (!hands.value.includes(score)) {
-    hands.value.push(score);
+const draw = (card: number) => {
+  if (!hands.value.includes(card)) {
+    hands.value.push(card);
     hands.value.sort((a, b) => a - b);
   }
 }
 const reset = () => {
   isReveal.value = false;
-  stackScores.value.splice(0);
+  votes.value.splice(0);
   average.value = 0;
 }
 const reveal = () => {
   isReveal.value = true;
-  average.value = stackScores.value.filter(v => v > 0).reduce((sum, element) => sum + element, 0) / stackScores.value.filter(v => v > 0).length;
+  average.value = votes.value.filter(v => v > 0).reduce((sum, element) => sum + element, 0) / votes.value.filter(v => v > 0).length;
 };
 
-const onDragStart = (score: number, event: any) => {
+const onDrag = (score: number, event: any) => {
   event.dataTransfer.setData('score', JSON.stringify(score));
 };
 
@@ -99,7 +99,7 @@ const onDrop = (event: any) => {
   play(JSON.parse(event.dataTransfer.getData('score')));
 };
 
-const stackScoresStyle = (index: number): StyleValue => {
+const votesStyle = (index: number): StyleValue => {
   const random1 = Math.floor(Math.random() * (40 + 1 - 50)) + 50;
   const random2 = Math.floor(Math.random() * (40 + 1 - 50)) + 50;
   const random3 = Math.floor(Math.random() * (0 + 1 - 180)) + 180;
